7c8b72482879e8861289559fd2538748df87f770
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-04-01 13:13:23 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitCleanUp::Application.config.secret_key_base = '0988f3c4f5f5d4e93fef442d91e61603017da7a85167f6b50fbcf68a8359e8cc67c56ae9aa7db27b10c7829cae6d8484578ce3927ddacbb41287076f2297dde5'
